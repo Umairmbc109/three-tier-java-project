1058e78893fe87b06263adcc3d1a3df6c07a4609
Umair <umair.khan@gmail.com>
2024-05-19 03:54:36 +0500

    updated HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        UserDAO userDAO = new UserDAO();
-        userDAO.getAllUsers();
+        DatabaseConnection dbConnection = new DatabaseConnection();
+        UserDAO userDAO = new UserDAO(dbConnection); // Pass DatabaseConnection object to UserDAO constructor
+
+        // Assuming getAllUsers() method exists in UserDAO
+        userDAO.getAllUsers(); // Call the getAllUsers() method
     }
 }
+
