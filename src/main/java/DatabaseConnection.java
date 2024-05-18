2297ff320028176ef5ba8ddea7c55d7952ae647c
root <root@ip-172-31-30-147.ap-southeast-1.compute.internal>
2024-05-18 21:30:49 +0000

    updated DatabaseConnection.java

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,25 +1,12 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
+import java.io.IOException;
 
 public class DatabaseConnection {
-    private Connection connection;
-
-    public DatabaseConnection() {
-        try {
-            Properties properties = new Properties();
-            properties.load(getClass().getClassLoader().getResourceAsStream("database.properties"));
-            String url = properties.getProperty("url");
-            String username = properties.getProperty("username");
-            String password = properties.getProperty("password");
-            connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException | NullPointerException e) {
-            System.out.println("Error connecting to the database: " + e.getMessage());
-        }
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
-}
+	    public void connect() {
+		            try {
+				                // Code that may throw IOException
+						//         } catch (IOException e) {
+						//                     e.printStackTrace();
+						//                             }
+						//                                 }
+						//                                 }
+						//
